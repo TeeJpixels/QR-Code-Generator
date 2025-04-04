1cd9b99dab29ab235660465f97748845a258dc66
Teej Akhauri <teejakh69@gmail.com>
2025-04-05 05:20:47 +0530

    Add files via upload

---
+++ QR_CODE(BETA).py
@@ -0,0 +1,22 @@
+import qrcode ##Importing qr code library
+website_link = input("Enter the website link: ") #Asks user for a link/text to generate its qr
+
+#asks user for qr dimensions
+a = int(input("Enter the version of QR code(1-40): "))
+b = int(input("Enter the Box size of QR code(1-5): "))
+c = int(input("Enter the Border size of QR code(1-5): "))
+
+##inputs qr dimensions
+qr = qrcode.QRCode(version = a, box_size = b, border = c)
+
+qr.add_data(website_link) ##link is added to qr code
+qr.make() ##qr code is generated using .make()
+
+##color for qr
+img = qr.make_image(fill_color = "black", back_color = "white")
+
+##save's qr
+img.save('youtube_qr.png')
+
+##print's the qr
+print("QR code generated and saved as youtube_qr.png")
